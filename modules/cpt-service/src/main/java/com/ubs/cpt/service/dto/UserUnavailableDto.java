b9293a31a4bc557f0784f2ed9ddd4700995a0731
amarpandav <amarpandav@gmail.com>
2024-08-24 12:50:07 +0200

    some inline comment

--- modules/cpt-service/src/main/java/com/ubs/cpt/service/dto/UserUnavailableDto.java
+++ modules/cpt-service/src/main/java/com/ubs/cpt/service/dto/UserUnavailableDto.java
@@ -2,6 +2,7 @@ package com.ubs.cpt.service.dto;
 
 import java.util.List;
 
+//TODO: Amar feedback: why both userAbsentCapacities and userBookedCapacities are part of this class?
 public record UserUnavailableDto(
         List<UserAvailableCapacityDto> userAbsentCapacities,
         List<UserBookedCapacityDto> userBookedCapacities
