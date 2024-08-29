be25b875c41bd19ae425de431e4b8b78580d15f2
wrosoftware <wrosoftware@gmail.com>
2024-08-29 03:21:11 +0200

    ci/cd fix

--- modules/cpt-web-app/src/main/java/com/ubs/cpt/web/rest/ApplicationController.java
+++ modules/cpt-web-app/src/main/java/com/ubs/cpt/web/rest/ApplicationController.java
@@ -1,10 +1,12 @@
 package com.ubs.cpt.web.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/application")
 public class ApplicationController {
