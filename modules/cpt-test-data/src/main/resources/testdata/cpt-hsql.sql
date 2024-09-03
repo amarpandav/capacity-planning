01edf9c3218acc48b1b6f33e1056c8ef62ec3684
amarpandav <amarpandav@gmail.com>
2024-09-03 21:29:35 +0200

    create dual table

--- modules/cpt-test-data/src/main/resources/testdata/cpt-hsql.sql
+++ modules/cpt-test-data/src/main/resources/testdata/cpt-hsql.sql
@@ -1,6 +1,7 @@
 /*
 Execute HSQL specific DB scripts here. We don't need below sequence but I added it only to test the code
 */
+create table dual (dummy varchar(1) );
 
 DROP sequence XXXXX_KEY_SEQ IF EXISTS;
 create sequence XXXXX_KEY_SEQ start with 1 increment by 1;
